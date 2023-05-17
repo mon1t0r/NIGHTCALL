6263343fbad3eaa6f14353fb9dad4d4785a06a0d
mon1tor <westxlebyshek@gmail.com>
2023-05-18 02:59:40 +0300

    Add project files.

---
+++ NIGHTCALL/main.cpp
@@ -0,0 +1,178 @@
+#include <windows.h>
+#include <gl/gl.h>
+#include <math.h>
+#include "visual.h"
+
+#pragma comment(lib, "opengl32.lib")
+
+#define M_PI 3.14159265359
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
+void DisableOpenGL(HWND, HDC, HGLRC);
+
+void WindowResize(int, int);
+
+float xScale;
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASSEX wcex;
+	HWND hwnd;
+	HDC hDC;
+	HGLRC hRC;
+	MSG msg;
+	BOOL bQuit = FALSE;
+	int time = 0;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_OWNDC;
+	wcex.lpfnWndProc = WindowProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"NIGHTCALL";
+	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);;
+
+
+	if (!RegisterClassEx(&wcex))
+		return 0;
+
+	hwnd = CreateWindowEx(0,
+		L"NIGHTCALL",
+		L"NIGHTCALL",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		720,
+		720,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	EnableOpenGL(hwnd, &hDC, &hRC);
+
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+	WindowResize(rect.right, rect.bottom);
+
+	while (!bQuit)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				bQuit = TRUE;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			DrawScene(time, xScale);
+
+			glFlush();
+			SwapBuffers(hDC);
+
+			time++;
+			Sleep(1);
+		}
+	}
+
+	DisableOpenGL(hwnd, hDC, hRC);
+
+	DestroyWindow(hwnd);
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+
+	case WM_DESTROY:
+		return 0;
+
+	case WM_SIZE:
+		WindowResize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_KEYDOWN:
+	{
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+	}
+	break;
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	return 0;
+}
+
+void EnableOpenGL(HWND hwnd, HDC* hDC, HGLRC* hRC)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+
+	int iFormat;
+
+	*hDC = GetDC(hwnd);
+
+	ZeroMemory(&pfd, sizeof(pfd));
+
+	pfd.nSize = sizeof(pfd);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW |
+		PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 24;
+	pfd.cDepthBits = 16;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+
+	iFormat = ChoosePixelFormat(*hDC, &pfd);
+
+	SetPixelFormat(*hDC, iFormat, &pfd);
+
+	*hRC = wglCreateContext(*hDC);
+
+	wglMakeCurrent(*hDC, *hRC);
+}
+
+void DisableOpenGL(HWND hwnd, HDC hDC, HGLRC hRC)
+{
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(hRC);
+	ReleaseDC(hwnd, hDC);
+}
+
+void WindowResize(int x, int y) {
+	glViewport(0, 0, x, y);
+	xScale = x / (float)y;
+	float sz = 1.0;
+	glLoadIdentity();
+	glFrustum(-xScale * sz, xScale * sz, -sz, sz, 10, 1000);
+}
---
+++ NIGHTCALL/visual.cpp
@@ -0,0 +1,73 @@
+#include <windows.h>
+#include <gl/gl.h>
+#include <math.h>
+
+#define FLOAT_MAX 1000.0000000000000000f
+
+void DrawGrid(int, float);
+
+void DrawScene(int time, float xScale)
+{
+	glPushMatrix();
+
+	glLineWidth(1.0f);
+	DrawGrid(time, xScale);
+
+	glPopMatrix();
+
+	glPushMatrix();
+	glLoadIdentity();
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glBegin(GL_TRIANGLE_STRIP);
+
+	glVertex2f(-1.0f, 0.0f);
+	glVertex2f(1.0f, 0.0f);
+	glVertex2f(-1.0f, 1.0f);
+	glVertex2f(1.0f, 1.0f);
+
+	glEnd();
+
+	glLineWidth(2.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_LINES);
+
+	glVertex2f(-1.0f, 0.0f);
+	glVertex2f(1.0f, 0.0f);
+
+	glEnd();
+
+	glPopMatrix();
+}
+
+void DrawGrid(int time, float xScale)
+{
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	glTranslatef(0.0f, -1.1f, -5.0f);
+	glRotatef(-89.3f, 1.0f, 0.0f, 0.0f);
+
+	glBegin(GL_LINES);
+
+	int xCount = 43;
+	float xDelta = xScale * 0.3f;
+
+	for (int i = -xCount; i <= xCount; ++i)
+	{
+		glVertex3f(i * xDelta, 9999.0f, 0.0f);
+		glVertex3f(i * xDelta, 0.0f, 0.0f);
+	}
+
+	float yDelta = 1.5f;
+	int yCount = 60;
+	float timeDelta = (time % 25) / 25.0f;
+
+	for (int i = -yCount; i < 0; ++i)
+	{
+		double y = (i + timeDelta) * -yDelta;
+		glVertex3f(-999.0, y, 0.0f);
+		glVertex3f(999.0, y, 0.0f);
+	}
+
+	glEnd();
+}
